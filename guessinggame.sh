8db51676450c6a0e1e8ce3c25dc91f13d9b7cb0f
Platelmin1o <davidtavaresmartins2@gmail.com>
2019-02-21 09:41:52 -0300

    guessinggame.sh finished

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,19 +1,27 @@
 function guessinggame {
+	files=$(ls)
+	for f in $files
+	do
+        	echo $f >> num_files.txt
+	done
 	count=$(cat num_files.txt | wc -l)
 	echo "Try to guess how many files there is in this diretory"
-	for try in $@
+	read answer
+	while [ $answer -ne $count ]
 	do
-		if [ $try -lt $count ]
+		if [ $answer -lt $count ]
 		then
-			echo "Your guess is lower than the right number. Try again" 
-		elif [ $try -gt $count ]
+			echo "Your guess is lower than the right number. Try again"
+			read answer 
+		elif [ $answer -gt $count ]
 		then
 			echo "Your guess is higher yhan the right number. Try again"
-		elif [ $try -eq $count ]
-		then
-			echo "You got it!!!!"
+			read answer
 		fi
 	done
+	echo "You got it!!!!"
+	rm num_files.txt
 }
+guessinggame 
 
-guessinggame 7
+
